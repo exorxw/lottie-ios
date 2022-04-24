a3174af770bc6cbef312bf6e060c746bbc08c31c
renxinwei <362922604@qq.com>
2022-04-24 11:22:27 +0800

    Update LOTLayerContainer.m

--- lottie-ios/Classes/AnimatableLayers/LOTLayerContainer.m
+++ lottie-ios/Classes/AnimatableLayers/LOTLayerContainer.m
@@ -156,10 +156,10 @@
         
       id<LOTImageCache> imageCache = [LOTCacheProvider imageCache];
       if (imageCache) {
-        image = [imageCache imageForKey:imagePath];
+        image = [imageCache lotCachedImageForKey:imagePath];
         if (!image) {
           image = [UIImage imageWithContentsOfFile:imagePath];
-          [imageCache setImage:image forKey:imagePath];
+          [imageCache setLOTCachedImage:image forKey:imagePath];
         }
       } else {
         image = [UIImage imageWithContentsOfFile:imagePath];
